29628ed2f014d3d81367430b501d7299464d7279
yves137 <oneg2981@gmail.com>
2023-10-03 12:48:36 +0200

    update project

--- src/CustomButtons.js
+++
@@ -1,8 +0,0 @@
-export function CustomButtons({btnID}){
-   return <button
-        onClick={() => alert(`You have clicked on Button ${btnID}`)}
-        className="border border-black bg-slate-300 py-2 px-5 hover:bg-slate-200 rounded-md"
-      >
-        Button {btnID}
-      </button>
-}
--- src/DisplayButtons.js
+++
@@ -1,7 +0,0 @@
-import {CustomButtons} from './CustomButtons'
-
-export function DisplayButtons(){
-   return <div className='flex justify-center space-x-5'>
-        {Array(3).fill(null).map((v,i)=> <CustomButtons btnID={i+1}/>)}
-    </div>
-}

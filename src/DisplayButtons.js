94bcf93a2e9a35b179775014814867f668109add
yves137 <oneg2981@gmail.com>
2023-10-05 15:38:51 +0200

    update project

--- src/DisplayButtons.js
+++ src/DisplayButtons.js
@@ -2,6 +2,6 @@ import {CustomButtons} from './CustomButtons'
 
 export function DisplayButtons(){
    return <div className='flex justify-center space-x-5'>
-        {Array(3).fill(null).map((v,i)=> <CustomButtons btnID={i+1}/>)}
+        {Array(3).fill(null).map((_,i)=> <CustomButtons key={i} btnID={i+1}/>)}
     </div>
 }
